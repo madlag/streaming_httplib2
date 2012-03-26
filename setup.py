d8148498e334e917d1bac7a98661892fe3835df1
Francois Lagunas <francois@stupeflix.com>
2012-03-26 15:38:46 +0200

    Incremented version tag, for release on pypi.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = open(os.path.join(here, 'README')).read()
 NEWS = open(os.path.join(here, 'CHANGELOG')).read()
 
 
-version = '0.7.3'
+version = '0.7.4'
 
 install_requires = [
     # List your project dependencies here.
